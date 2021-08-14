5cf04f6d62f34ce020a13b714887ea1275362ceb
xrSanya <grigorjew.shura2011@yandex.ru>
2021-08-14 15:16:53 +0300

    Добавлены ViewModel для окон

---
+++ ViewModels/ProcessPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Light.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Light.ViewModels
+{
+    public sealed class ProcessPageViewModel : ViewModelBase
+    {
+        public ProcessPageViewModel()
+        {
+
+        }
+    }
+}
