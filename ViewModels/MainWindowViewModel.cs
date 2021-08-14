86ec4bf2548be02a27fcee436edb4c3f4fd6444e
xrSanya <grigorjew.shura2011@yandex.ru>
2021-08-14 15:28:10 +0300

    Связал ViewModel и View

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,51 @@
-﻿using Light.ViewModels.Base;
+﻿using Light.Commands;
+using Light.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Light.ViewModels
 {
     public sealed class MainWindowViewModel : ViewModelBase
     {
+        #region Values
+
+        private string _currentTime = "12:00";
+
+        #endregion
+
+        #region Constructors
+
+        public string CurrentTime
+        {
+            get => _currentTime;
+            set => Set(ref _currentTime, value);
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand CloseAppCommand { get; }
+        public ICommand AppToTrayCommand { get; }
+        public ICommand OpenSettingsWindowCommand { get; }
+
+        private void OnCloseAppCommandExecute() { /* in progress */ }
+
+        private void OnAppToTrayCommandExecute() { /* in progress */ }
+
+        private void OnOpenSettingsWindowExecute() { /* in progress */ }
+
+        #endregion
+
         public MainWindowViewModel()
         {
-
+            OpenSettingsWindowCommand = new LambdaCommand(p => OnOpenSettingsWindowExecute());
+            CloseAppCommand = new LambdaCommand(p => OnCloseAppCommandExecute());
+            AppToTrayCommand = new LambdaCommand(p => OnAppToTrayCommandExecute());
         }
     }
 }
