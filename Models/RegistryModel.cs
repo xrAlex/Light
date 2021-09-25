9580b4c406837628603aec96a575a414c7e4da87
Alex Grigorjew <grigorjew.shura2011@yandex.ru>
2021-09-25 03:18:55 +0300

    Добавлена возможность работы с регистром используя CMD чтобы приложению не требовался запуск от администатора

--- Models/RegistryModel.cs
+++ Models/RegistryModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,24 +11,59 @@ using Microsoft.Win32;
 
 namespace Light.Models
 {
+    /// <summary>
+    /// Модель для работы с записями в регистре
+    /// </summary>
     public class RegistryModel
     {
-        private const string AppNameKey = "Light";
+        private static readonly string AppNameKey = $"{AppDomain.CurrentDomain.FriendlyName}";
         private const string StartupPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
-        private const string GammaRangeKey = "GdiICMGammaRange";
-        private const string GammaRangePath = "Software\\Microsoft\\Windows NT\\CurrentVersion\\ICM";
+        private const string GetGammaRangeValue = "REG QUERY \"HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion\\ICM\" /v GdiICMGammaRange";
+        private const string SetDefaultGammaRangeValue = "REG ADD \"HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion\\ICM\" /v GdiICMGammaRange /t REG_DWORD /d 0 /f";
+        private const string SetExtendedGammaRangeValue = "REG ADD \"HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion\\ICM\" /v GdiICMGammaRange /t REG_DWORD /d 256 /f";
 
         public bool IsAppStartupKeyFounded() => IsRegistryKeyFounded(Registry.CurrentUser, StartupPath, AppNameKey) != null;
-        public bool IsExtendedGammaRangeActive() 
-        {
-            var value = IsRegistryKeyFounded(Registry.LocalMachine, GammaRangePath, GammaRangeKey);
-            return value != null && Convert.ToInt32(value) == 256;
-        }
-
         public void DeleteAppStartupKey() => DeleteKey(Registry.CurrentUser, StartupPath, AppNameKey);
         public void AddAppStartupKey() => AddKey(Registry.CurrentUser, StartupPath, AppNameKey, GetAppExecutingLocation);
-        public void SetDefaultGammaRangeKey() => DeleteKey(Registry.LocalMachine, GammaRangePath, GammaRangeKey);
-        public void SetExtendedGammaRangeKey() => AddKey(Registry.LocalMachine, GammaRangePath, GammaRangeKey, 256);
+
+        public bool IsExtendedGammaRangeActive() 
+        {
+            var output = ExecuteFromCMD(GetGammaRangeValue, false, true);
+            return output.Contains("0x100");
+        }
+
+        public void SetDefaultGammaRangeKey() => ExecuteFromCMD(SetDefaultGammaRangeValue, true, false);
+        public void SetExtendedGammaRangeKey() => ExecuteFromCMD(SetExtendedGammaRangeValue, true, false);
+
+        /// <summary>
+        /// Исполняет команду используя CMD
+        /// </summary>
+        /// <remarks> Используется для того что бы не запрашивать права администратора при запуске программы </remarks>
+        /// <returns> Если redirectOutput = true результат исполнения команды </returns>
+        private string ExecuteFromCMD(string command, bool shellExecute, bool redirectOutput)
+        {
+            var output = "";
+            var processStartInfo = new ProcessStartInfo
+            {
+                UseShellExecute = shellExecute,
+                FileName = "CMD.exe",
+                Arguments = "/C " + command,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                Verb = "runas",
+                RedirectStandardOutput = !shellExecute && redirectOutput
+            };
+
+            using var cmdProcess = Process.Start(processStartInfo);
+            if (redirectOutput)
+            {
+                using var reader = cmdProcess?.StandardOutput;
+                output = reader?.ReadToEnd();
+            }
+            cmdProcess?.WaitForExit();
+
+            return output;
+        }
         private RegistryKey GetSubKey(RegistryKey key, string subKey) => key.OpenSubKey(subKey, true);
 
         private object IsRegistryKeyFounded(RegistryKey key, string subKey, string valueName)
